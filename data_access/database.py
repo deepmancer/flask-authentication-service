5c6ed55abd21214469fec1567212b35ad2e08d25
Alireza <59364943+deepmancer@users.noreply.github.com>
2024-08-15 20:25:17 +0330

    Update database.py

--- data_access/database.py
+++ data_access/database.py
@@ -1,110 +1,54 @@
-import asyncio
-import sys
 from pymongo import MongoClient
-import srvlookup
-from mongoengine import *
-import dns.resolver
+from mongoengine import Document, connect, IntField, StringField
 
-database = None
-
-
-# ************* create user and login database and tables *************
+DATABASE_URI = 'mongodb+srv://client:password1379@cluster0.pcb8q.mongodb.net/database?retryWrites=true&w=majority'
 
 class LoginLog(Document):
-    loginsCount = IntField(required=True)
-    userId = StringField(required=True)
+    logins_count = IntField(required=True)
+    user_id = StringField(required=True)
     token = StringField(required=True)
     meta = {'collection': 'loginLogs'}
 
-
 class RegisterLog(Document):
-    userId = StringField(required=True)
+    user_id = StringField(required=True)
     password = StringField(required=True)
     meta = {'collection': 'users'}
 
+async def connect():
+    connect('database', host=DATABASE_URI)
 
-async def connectToDatabases():
-    return connect('database',
-                   host='mongodb+srv://client:password1379@cluster0.pcb8q.mongodb.net/database?retryWrites=true&w'
-                        '=majority')
+async def print_logins_collection():
+    print("********** Logins Collection **********")
+    for log in LoginLog.objects:
+        print(f"Logins Count: {log.logins_count} | User ID: {log.user_id} | Token: {log.token}")
 
+async def print_users_collection():
+    print("********** Users Collection **********")
+    for user in RegisterLog.objects:
+        print(f"User ID: {user.user_id} | Password: {user.password}")
 
-async def printLoginsLogCollection():
-    try:
-        print("********** logins log collection **********\n")
-        for loginLogObject in LoginLog.objects:
-            print(
-                "loginsCount: " + str(loginLogObject.loginsCount) + " | userId: " + loginLogObject.userId + " | token: "
-                + loginLogObject.token + " \n")
+async def record_login(user_id, token):
+    log_count = LoginLog.objects.count()
+    LoginLog(logins_count=log_count + 1, user_id=user_id, token=token).save()
+    print("Login recorded")
 
-    except:
-        print("can not print logins log collection")
+async def get_last_token(user_id):
+    logs = LoginLog.objects(user_id=user_id).order_by('-logins_count')
+    return logs.first().token if logs else None
 
+async def get_user_id_by_token(token):
+    log = LoginLog.objects(token=token).first()
+    return log.user_id if log else None
 
-async def printUsersCollection():
-    try:
-        print("********** users collection **********")
-        for registerObject in RegisterLog.objects:
-            print("userId: " + registerObject.userId + " | password: " + str(registerObject.password) + "\n")
+async def get_password(user_id):
+    user = RegisterLog.objects(user_id=user_id).first()
+    return user.password if user else None
 
-    except:
-        print("can not print users collection")
+async def is_token_valid(token):
+    return LoginLog.objects(token=token).count() > 0
 
+async def does_user_exist(user_id):
+    return RegisterLog.objects(user_id=user_id).count() > 0
 
-# ************* logins functions *************
-
-
-async def submitLogin(userId, token):
-    loginsCount = len(LoginLog.objects)
-    primaryKey = loginsCount + 1
-    loginLog = LoginLog(loginsCount=primaryKey, userId=userId, token=token).save()
-    print("login submitted")
-
-
-async def getUserLastToken(id):
-    try:
-        userLogs = (LoginLog.objects(userId=id))
-        userLastToken = userLogs[len(userLogs) - 1].token
-        return userLastToken
-    except:
-        return None
-
-
-async def getUserIdWithToken(token):
-    try:
-        userId = LoginLog.objects(token=token)[0].userId
-        return userId
-    except:
-        return None
-
-
-# ************* user password stuff *************
-
-
-async def getPasswordForThisId(id):
-    try:
-        encryptedPassword = RegisterLog.objects(userId=id)[0].password
-        return encryptedPassword
-    except:
-        return None
-
-
-async def isThisTokenAvailable(token):
-    try:
-        if len(LoginLog.objects(token=token)) != 0:
-            return True
-        return False
-    except:
-        return False
-
-
-async def isThereUserWithId(id):
-    try:
-        user = RegisterLog.objects(userId=id)[0]
-        return True
-    except:
-        return False
-
-
-async def registerUser(id, password):
-    user = RegisterLog(userId=id, password=password).save()
+async def register_user(user_id, password):
+    RegisterLog(user_id=user_id, password=password).save()
