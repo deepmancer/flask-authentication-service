326d10293f407c0ae8411c15d6300456d307213b
Alireza <59364943+deepmancer@users.noreply.github.com>
2024-08-15 20:24:14 +0330

    cleaned

--- server.py
+++ server.py
@@ -1,47 +1,36 @@
 from flask import Flask, request, jsonify
 from model import controller
-import asyncio
 
 app = Flask(__name__)
 
+@app.post('/login')
+async def login():
+    data = request.get_json()
+    response_data = await controller.login_user(data["id"], data["password"])
+    return jsonify(response_data)
 
-@app.route('/login', methods=['POST'])
-def login():
-    if request.method == 'POST':
-        data = request.get_json()
-        responseData = asyncio.run(controller.loginUser(data["id"], data["password"]))
-        return jsonify(responseData)
+@app.post('/register')
+async def register():
+    data = request.get_json()
+    response_data = await controller.register_user(data["id"], data["password"])
+    return jsonify(response_data)
 
+@app.post('/getUserIdWithToken')
+async def get_user_id_with_token():
+    data = request.get_json()
+    response_data = await controller.get_user_id_with_token(data["token"])
+    return jsonify(response_data)
 
-@app.route('/register', methods=['POST'])
-def register():
-    if request.method == 'POST':
-        data = request.get_json()
-        responseData = asyncio.run(controller.registerUser(data["id"], data["password"]))
-        return jsonify(responseData)
+@app.post('/isTokenExpired')
+async def is_token_expired():
+    data = request.get_json()
+    is_expired = await controller.is_token_expired(data["token"])
+    return jsonify({"isExpired": is_expired})
 
-
-@app.route('/getUserIdWithToken', methods=['POST'])
-def getId():
-    if request.method == 'POST':
-        data = request.get_json()
-        responseData = asyncio.run(controller.getUserIdWithToken(data["token"]))
-        return jsonify(responseData)
-
-
-@app.route('/isTokenExpired', methods=['POST'])
-def checkTokenExpired():
-    if request.method == 'POST':
-        data = request.get_json()
-        responseData = asyncio.run(controller.isTokenExpired(data["token"]))
-        finalResponse = str(responseData)
-        return jsonify({"isExpired": finalResponse})
-
-
-async def runServer():
-    await controller.runDatabase()
-    await app.run(host="localhost", port=9999, debug=False)
-
+async def run_server():
+    await controller.initialize_database()
+    await app.run_task(host="localhost", port=9999, debug=False)
 
 if __name__ == '__main__':
-    asyncio.run(runServer())
+    import asyncio
+    asyncio.run(run_server())
